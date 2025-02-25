f28359ceacd05a8823b5e4e0dc47369a16de177c
Daniel Jin <mrpickles123115@gmail.com>
2025-02-24 21:45:34 -0700

    seperate extension class for areas

--- window/extensions/icon_container/icon_container.gd
+++ window/extensions/icon_container/icon_container.gd
@@ -1,30 +1,17 @@
-extends WindowExtension
+extends ResizeableArea
 class_name IconContainer
 
 @onready var area := $Area2D as Area2D
 @onready var col_shape : CollisionShape2D = $Area2D/CollisionShape2D
 
-@export var pin_right : bool = true
-@export var pin_top : bool = true
-@export var y_diff : int = 20
-@export var other_y_diff : int = 5
-
-@export var x_diff : int = 5
-@export var other_x_diff : int = 5
-
-@export var x_scale : float = 1.0
-@export var y_scale : float = 1.0
-
 @export var icon_scale = 0.5
 @export var spacing_x : int = 18
 
-var r : RectangleShape2D
 var inside : Array[CollisionObject2D] = []
+var inner_shape : RectangleShape2D
 
 func _ready():
 	super._ready()
-	r = RectangleShape2D.new()
-
 	clip_children = ClipChildrenMode.CLIP_CHILDREN_AND_DRAW
 	#clip_children = ClipChildrenMode.CLIP_CHILDREN_ONLY
 
@@ -33,8 +20,9 @@ func _ready():
 
 	area.connect("body_entered", _on_body_entered)
 	area.connect("body_exited", _on_body_exited)
+	inner_shape = RectangleShape2D.new()
 
-	col_shape.shape = r
+	col_shape.shape = inner_shape
 	update_icons()
 
 
@@ -54,31 +42,21 @@ func update_icons() -> void:
 	var x : int = 0
 	for icon in icon_list:
 		var offset = Vector2(x, 0)
-		icon.position = offset + area.position - r.size / 2 + Vector2(spacing_x, spacing_x) / 2
+		icon.position = offset + area.position - boundary.size / 2 + Vector2(spacing_x, spacing_x) / 2
 		x += spacing_x
 
 func window_size_changed(w, h) -> void:
 	super.window_size_changed(w, h)
-	var new_size := Vector2(x_scale * (w - (x_diff + other_x_diff)), y_scale * (h - (y_diff + other_y_diff)))
-	r.size = new_size
-
-	if pin_top:
-		area.position.y = (new_size.y / 2) + y_diff
-	else:
-		area.position.y = (h - new_size.y / 2) - y_diff
-
-	if pin_right:
-		area.position.x = (w - new_size.x / 2) - x_diff
-	else:
-		area.position.x = new_size.x / 2 + x_diff
+	area.position = boundary.position
+	inner_shape.size = boundary.size
 
 	queue_redraw()
 
 func _draw() -> void:
 	var outer := Rect2()
 
-	outer.size = r.size
-	outer.position = area.position - r.size / 2
+	outer.size = boundary.size
+	outer.position = area.position - boundary.size / 2
 
 	
 	draw_rect(outer, Color.GRAY)
--- window/extensions/resizeable_area.gd
+++ window/extensions/resizeable_area.gd
@@ -1,9 +1,6 @@
 extends WindowExtension
 class_name ResizeableArea
 
-@onready var area := $Area2D as Area2D
-@onready var col_shape : CollisionShape2D = $Area2D/CollisionShape2D
-
 @export var pin_right : bool = true
 @export var pin_top : bool = true
 @export var y_diff : int = 20
@@ -15,28 +12,22 @@ class_name ResizeableArea
 @export var x_scale : float = 1.0
 @export var y_scale : float = 1.0
 
-var r : RectangleShape2D
-var inside : Array[CollisionObject2D] = []
-
-func _ready():
-	super._ready()
-	r = RectangleShape2D.new()
-
+var boundary : Rect2 = Rect2()
 
 func window_size_changed(w, h) -> void:
 	super.window_size_changed(w, h)
 	var new_size := Vector2(x_scale * (w - (x_diff + other_x_diff)), y_scale * (h - (y_diff + other_y_diff)))
-	r.size = new_size
+	boundary.size = new_size
 
 	if pin_top:
-		area.position.y = (new_size.y / 2) + y_diff
+		boundary.position.y = (new_size.y / 2) + y_diff
 	else:
-		area.position.y = (h - new_size.y / 2) - y_diff
+		boundary.position.y = (h - new_size.y / 2) - y_diff
 
 	if pin_right:
-		area.position.x = (w - new_size.x / 2) - x_diff
+		boundary.position.x = (w - new_size.x / 2) - x_diff
 	else:
-		area.position.x = new_size.x / 2 + x_diff
+		boundary.position.x = new_size.x / 2 + x_diff
 
 	queue_redraw()
 
